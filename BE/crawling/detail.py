8712019ce726745727fc374fa9d5771501944664
PoloCeleste <voidness12@gmail.com>
2025-03-20 21:02:10 +0900

    [#S12P21E202-37] Feat: 상품 초기 데이터 수집

--- BE/crawling/detail.py
+++ BE/crawling/detail.py
@@ -3,7 +3,7 @@ from dbctl import DBs
 from time import sleep
 from pprint import pprint
 from driver import init_driver
-from mj import make_json, open_json
+from mj import make_json # , open_json
 from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
@@ -13,12 +13,14 @@ from selenium.webdriver.support import expected_conditions as EC
 keyboard = Controller()
 exit_flag = False
 
+
 def on_press(key):
     global exit_flag
     if key == Key.esc:
         print("\nESC 키가 눌렸습니다. 프로그램을 종료합니다.")
         exit_flag = True
         return False
+
 
 def listen_for_esc():
     with Listener(on_press=on_press) as listener:
@@ -27,6 +29,7 @@ def listen_for_esc():
     with Listener(on_press=on_press) as listener:
         listener.join()
 
+
 def strToDict(data):
     try:
         parsed_data = json.loads(data)
@@ -34,24 +37,27 @@ def strToDict(data):
     except json.JSONDecodeError as e:
         print(f"JSON 파싱 오류: {e}")
 
+
 if __name__ == '__main__':
+    # 별도 프로세스로 ESC 눌림 감지
     esc_listener_thread = threading.Thread(target=listen_for_esc, daemon=True)
     esc_listener_thread.start()
-    # while True:
-    #     try:
-    #         KeyWi=DBs()
-    #         if KeyWi:break
-    #     except:
-    #         print("다시 시도해주세요.")
-    # products = KeyWi.select_product(800,800)
     
-    products = open_json()
+    while True:  # DB 로그인
+        try:
+            KeyWi=DBs()
+            if KeyWi:break
+        except:
+            print("다시 시도해주세요.")
     
-    fail_products={}
-    result=[]
-    # for products_id, *value in products:
-    for products_id, value in products.items():
-        if exit_flag: break
+    products = KeyWi.select_product()  # 상품 DB에서 가져오기
+    # products = open_json()  # 실패 목록에서 가져오기
+    
+    result=[]  # 성공 목록
+    fail_products={}  # 실패 목록
+    for products_id, *value in products:  # db에서 정보 가져와 크롤링
+    # for products_id, value in products.items():  # 실패한 상품 목록에서 정보 가져와 크롤링
+        if exit_flag: break  # esc 눌리면 크롤링 종료
         try:
             driver, wait = init_driver()
             category_id, product_name, price, product_url, product_image, options = value["value"]
@@ -73,9 +79,14 @@ if __name__ == '__main__':
                     act.move_to_element(moredetail).perform()
                     sleep(0.5)
                     act.click(moredetail).perform()
+            
             except Exception as e:
                 print(e)
                 print("상세 보기 실패")
+                try: body.find_element(By.XPATH, "//strong[text()='이 상품은 현재 판매중지 된 상품입니다.']")
+                except: print("로딩 실패")
+                else: print("이 상품은 현재 판매중지 된 상품입니다.")
+            
             
             productdetails = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, '_9F9CWn02VE')))
             
@@ -127,26 +138,31 @@ if __name__ == '__main__':
                 elif "se-oembed " in compclass:
                     embedURL=component.find_element(By.TAG_NAME, "iframe").get_attribute("src")
                     description_list.append([products_id, embedURL, len(description_list)+1, 'embed'])
-        
-        
-            # for description in description_list:
-            #     KeyWi.insert_product_description(*description)
+
+            for description in description_list:
+                KeyWi.insert_product_description(*description)
             print(product_name)
             pprint(description_list)
             print()
             driver.quit()
+        
         except Exception as e:
             print(products_id, len(description_list)+1)
             print(e)
-            # fail_products[products_id]={"value":value,"error_type": e.__class__.__name__, "error_message":str(e), "location":(len(description_list)+1)}
-            fail_products[products_id]={"value":value["value"],"error_type": e.__class__.__name__, "error_message":str(e), "location":(len(description_list)+1)}
+            # DB에서 상품 조회 시
+            fail_products[products_id]={"value":value,"error_type": e.__class__.__name__, "error_message":str(e), "location":(len(description_list)+1)}
+            # 실패 목록에서 상품 조회 시
+            # fail_products[products_id]={"value":value["value"],"error_type": e.__class__.__name__, "error_message":str(e), "location":(len(description_list)+1)}
             driver.quit()
-        else:
-            result=result+description_list
-        finally:description_list=[]
+        
+        else: result=result+description_list
+        
+        finally: description_list=[]
+    
     if result:
-        try:make_json('products_descriptions', result)
-        except Exception as e:print(e)
+        try: make_json('products_descriptions', result)
+        except Exception as e: print(e)
+    
     if fail_products:
-        try:make_json('fail_products', fail_products)
-        except Exception as e:print(e)
+        try: make_json('fail_products', fail_products)
+        except Exception as e: print(e)
