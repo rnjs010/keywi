4bbd9c9cc1f9db6dc593a033cc4233342120c724
PoloCeleste <voidness12@gmail.com>
2025-03-26 17:59:10 +0900

    [#S12P21E202-103] Docs: 250326 Expo 앱 빌드

--- documents/joochan/0325/KeyWi/app/index.tsx
+++ documents/joochan/0325/KeyWi/app/index.tsx
@@ -1,5 +1,32 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
 import { WebView } from "react-native-webview";
 
-export default function App() {
-  return <WebView source={{ uri: "https://keywi.poloceleste.site" }} />;
-}
+const MyWebView = () => {
+  return (
+    <View style={styles.container}>
+      <WebView
+        source={{ uri: "https://keywi.poloceleste.site" }}
+        style={styles.webview}
+        scrollEnabled={false}
+        nestedScrollEnabled={false}
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+        bounces={false}
+        overScrollMode="never"
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    overflow: "hidden",
+  },
+  webview: {
+    flex: 1,
+  },
+});
+
+export default MyWebView;
