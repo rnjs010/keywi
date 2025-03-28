ec0f11083d0467423ec632c4aa6f482662c5e294
PoloCeleste <voidness12@gmail.com>
2025-03-28 12:46:28 +0900

    [#S12P21E202-105][INFRA] Fix: config https적용 주소로 연결 & 타임존 설정

--- BE/eureka/src/main/java/com/ssafy/eureka/EurekaApplication.java
+++ BE/eureka/src/main/java/com/ssafy/eureka/EurekaApplication.java
@@ -3,12 +3,14 @@ package com.ssafy.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(EurekaApplication.class, args);
 	}
 
