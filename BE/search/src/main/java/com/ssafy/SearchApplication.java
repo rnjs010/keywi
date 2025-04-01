456074d3cac1aaf600e99982d389acccdbb9f3d8
이수환 <qkqhzz120@naver.com>
2025-04-01 09:57:27 +0900

    Chore: EnableFeignClients 제거

--- BE/search/src/main/java/com/ssafy/SearchApplication.java
+++ BE/search/src/main/java/com/ssafy/SearchApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = "com.ssafy.integratedSearch.client")
+//@EnableFeignClients(basePackages = "com.ssafy.integratedSearch.client")
 public class SearchApplication {
 
     public static void main(String[] args) {
