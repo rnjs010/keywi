125dcfdb3c5e2c11d565ff6dba143a70b0ff289a
PoloCeleste <voidness12@gmail.com>
2025-04-03 09:19:16 +0900

    [#S12P21E202-113][BE] Fix: RefreshScope 추가, bootstrap.yml 추가

--- BE/search/src/main/java/com/ssafy/config/ElasticsearchConfig.java
+++ BE/search/src/main/java/com/ssafy/config/ElasticsearchConfig.java
@@ -8,9 +8,10 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+@RefreshScope
 @Configuration
 public class ElasticsearchConfig {
 
