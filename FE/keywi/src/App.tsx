f33e373261d1833e51d8bc3f038a029feb6a3182
PoloCeleste <voidness12@gmail.com>
2025-04-03 16:25:55 +0900

    [#S12P21E202-63][FE] Minor: 로그인 테스트

--- FE/keywi/src/App.tsx
+++ FE/keywi/src/App.tsx
@@ -8,15 +8,15 @@ import MainPage from './pages/login/MainPage'
 import HomePage from './pages/home/HomePage'
 import HomeCommentPage from './pages/home/HomeCommentPage'
 // NOTE - 로그인 테스트 원할 시 주석해제
-// import { Route, Routes, Outlet, Navigate } from 'react-router-dom'
-import { Route, Routes, Navigate } from 'react-router-dom'
+import { Route, Routes, Outlet, Navigate } from 'react-router-dom'
+// import { Route, Routes, Navigate } from 'react-router-dom'
 import PayPage from './pages/pay/PayPage'
 import HomeImgSelectPage from './pages/home/HomeImgSelectPage'
 import HomeTagPage from './pages/home/HomeTagPage'
 import HomeWritePage from './pages/home/HomeWritePage'
 import KakaoHandler from './features/login/hooks/KakaoHandler'
 //NOTE - 로그인 테스트 원할 시 주석해제
-// import { ProtectedRoute } from './components/ProtectedRoute'
+import { ProtectedRoute } from './components/ProtectedRoute'
 import MyPage from './pages/mypage/MyPage'
 import { SettingPage } from './pages/mypage/SettingPage'
 import { SettingAccountPage } from './pages/mypage/SettingAccountPage'
@@ -37,39 +37,42 @@ function App() {
         <Route path="/callback/kakao" element={<KakaoHandler />} />
         {/* //NOTE - 로그인 테스트 원할 시 주석해제 */}
         {/* 보호된 라우트 - 인증 필요 */}
-        {/* <Route
+        <Route
           element={
             <ProtectedRoute>
               <Outlet />
             </ProtectedRoute>
           }
-        > */}
-        {/* 홈 관련 라우트 */}
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/home/comment/:feedId" element={<HomeCommentPage />} />
-        <Route path="/home/imgselect" element={<HomeImgSelectPage />} />
-        <Route path="/home/tag" element={<HomeTagPage />} />
-        <Route path="/home/write" element={<HomeWritePage />} />
+        >
+          {/* 홈 관련 라우트 */}
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/home/comment/:feedId" element={<HomeCommentPage />} />
+          <Route path="/home/imgselect" element={<HomeImgSelectPage />} />
+          <Route path="/home/tag" element={<HomeTagPage />} />
+          <Route path="/home/write" element={<HomeWritePage />} />
 
-        {/* 게시판 관련 라우트 */}
-        <Route path="/board" element={<BoardPage />} />
-        <Route path="/board/:postId" element={<BoardDetailPage />} />
-        <Route path="/board/write" element={<BoardWritePage />} />
+          {/* 게시판 관련 라우트 */}
+          <Route path="/board" element={<BoardPage />} />
+          <Route path="/board/:postId" element={<BoardDetailPage />} />
+          <Route path="/board/write" element={<BoardWritePage />} />
 
-        {/* 결제 라우트 */}
-        <Route path="/pay" element={<PayPage />} />
+          {/* 결제 라우트 */}
+          <Route path="/pay" element={<PayPage />} />
 
-        {/* 마이페이지 라우트 */}
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/setting" element={<SettingPage />} />
-        <Route path="/setting/account" element={<SettingAccountPage />} />
+          {/* 마이페이지 라우트 */}
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/setting" element={<SettingPage />} />
+          <Route path="/setting/account" element={<SettingAccountPage />} />
 
-        {/* 채팅페이지 라우트 */}
-        <Route path="/chat" element={<ChatListPage />} />
-        <Route path="/chat/:roomId" element={<ChatRoomPage />} />
-        <Route path="/chat/:roomId/dealrequest" element={<DealRequestPage />} />
-        <Route path="/chat/:roomId/dealaccept" element={<DealAcceptPage />} />
-        {/* </Route> */}
+          {/* 채팅페이지 라우트 */}
+          <Route path="/chat" element={<ChatListPage />} />
+          <Route path="/chat/:roomId" element={<ChatRoomPage />} />
+          <Route
+            path="/chat/:roomId/dealrequest"
+            element={<DealRequestPage />}
+          />
+          <Route path="/chat/:roomId/dealaccept" element={<DealAcceptPage />} />
+        </Route>
 
         {/* 404 페이지나 기타 예외 처리 */}
         <Route path="*" element={<Navigate to="/" replace />} />
