2a914a8b6451925fdb20a08d2575807f36c5cea8
PoloCeleste <voidness12@gmail.com>
2025-04-04 11:11:29 +0900

    [#S12P21E202-62][BE] Minor: main 수정

--- BE/product/src/main/java/com/ssafy/product/ProductApplication.java
+++ BE/product/src/main/java/com/ssafy/product/ProductApplication.java
@@ -3,10 +3,12 @@ package com.ssafy.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.ssafy.product.mapper")
 public class ProductApplication {
 
