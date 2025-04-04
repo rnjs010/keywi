cca1de8a9c7dd2f346dec0d55a0b031a27c05b7c
이수환 <qkqhzz120@naver.com>
2025-04-04 11:11:13 +0900

    Chore: EnableDiscoveryClient 어노테이션 추가

--- BE/search/src/main/java/com/ssafy/search/SearchApplication.java
+++ BE/search/src/main/java/com/ssafy/search/SearchApplication.java
@@ -3,11 +3,13 @@ package com.ssafy.search;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchClientAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import java.util.TimeZone;
 
 @SpringBootApplication(exclude = ElasticsearchClientAutoConfiguration.class)
 @EnableScheduling
+@EnableDiscoveryClient
 public class SearchApplication {
 
     public static void main(String[] args) {
