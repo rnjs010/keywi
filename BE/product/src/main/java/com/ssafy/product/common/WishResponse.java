7ff949775f42d920628e4ecd0bc45e7f806751fe
PoloCeleste <voidness12@gmail.com>
2025-04-04 13:59:00 +0900

    [#S12P21E202-62][BE] Fix 반환 수정

--- BE/product/src/main/java/com/ssafy/product/common/WishResponse.java
+++ BE/product/src/main/java/com/ssafy/product/common/WishResponse.java
@@ -8,17 +8,17 @@ public class WishResponse<T> {
 
     private String status;    // 상태 (success, error)
     private String message;   // 메시지
-    private T isFavorite;           // 응답 데이터
+    private T Data;           // 응답 데이터
 
     // 기본 생성자
     public WishResponse() {
     }
 
     // 모든 필드를 초기화하는 생성자
-    public WishResponse(String status, String message, T isFavorite) {
+    public WishResponse(String status, String message, T Data) {
         this.status = status;
         this.message = message;
-        this.isFavorite = isFavorite;
+        this.Data = Data;
     }
 
     // Getter 및 Setter 메서드
@@ -38,22 +38,22 @@ public class WishResponse<T> {
         this.message = message;
     }
 
-    public T getIsFavorite() {
-        return isFavorite;
+    public T getData() {
+        return Data;
     }
 
-    public void setIsFavorite(T isFavorite) {
-        this.isFavorite = isFavorite;
+    public void setData(T Data) {
+        this.Data = Data;
     }
 
     /**
      * 성공 응답 메시지 생성
      * @param message 성공 메시지
-     * @param isFavorite 응답 데이터
+     * @param Data 응답 데이터
      * @return 성공 응답 메시지 객체
      */
-    public static <T> WishResponse<T> success(String message, T isFavorite) {
-        return new WishResponse<>("success", message, isFavorite);
+    public static <T> WishResponse<T> success(String message, T Data) {
+        return new WishResponse<>("success", message, Data);
     }
 
     /**
