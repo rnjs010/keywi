24001a3d9d1c33c3a367f7be6c37d7edd6abf815
PoloCeleste <voidness12@gmail.com>
2025-04-07 10:07:13 +0900

    [#S12P21E202-113][INFRA] Minor: 배포 세팅

--- BE/chat/src/main/java/com/ssafy/chat/ChatApplication.java
+++ BE/chat/src/main/java/com/ssafy/chat/ChatApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 public class ChatApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(ChatApplication.class, args);
 	}
 
