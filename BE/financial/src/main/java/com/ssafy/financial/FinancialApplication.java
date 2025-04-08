8ecc39d9be4271af901b4f820ae55a366298bff5
이수환 <qkqhzz120@naver.com>
2025-04-08 12:14:56 +0900

    Chore: 타임존 한국시간으로 변경

--- BE/financial/src/main/java/com/ssafy/financial/FinancialApplication.java
+++ BE/financial/src/main/java/com/ssafy/financial/FinancialApplication.java
@@ -3,10 +3,13 @@ package com.ssafy.financial;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class FinancialApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(FinancialApplication.class, args);
     }
 
