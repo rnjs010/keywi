c9dea72c140c62116cf23dc9be34dd90b34a8daf
이수환 <qkqhzz120@naver.com>
2025-04-09 03:10:27 +0900

    [#S12P21E202-154][BE] Refacto: 유저키 생성 코드 수정

--- BE/financial/src/main/java/com/ssafy/financial/repository/UserAccountConnectionRepository.java
+++ BE/financial/src/main/java/com/ssafy/financial/repository/UserAccountConnectionRepository.java
@@ -12,9 +12,9 @@ import java.util.Optional;
 public interface UserAccountConnectionRepository extends JpaRepository<UserAccountConnectionEntity, Long> {
 
     boolean existsByUserAndDemandAccount(UsersEntity user, AccountEntity account);
-    Optional<UserAccountConnectionEntity> findByUserAndDemandAccount(UsersEntity user, AccountEntity account);
+//    Optional<UserAccountConnectionEntity> findByUserAndDemandAccount(UsersEntity user, AccountEntity account);
     Optional<UserAccountConnectionEntity> findTopByUserIdOrderByConnectedAtDesc(Long userId);
     Optional<UserAccountConnectionEntity> findByUserId(Long userId);
-//    Optional<UserAccountConnectionEntity> findTopByUser_UserIdOrderByConnectedAtDesc(Long userId);
+    Optional<UserAccountConnectionEntity> findTopByUser_IdOrderByConnectedAtDesc(Long userId);
 
 }
