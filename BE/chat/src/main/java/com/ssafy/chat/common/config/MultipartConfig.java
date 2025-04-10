72c5d681b3e25774f12a2d119469367300977052
PoloCeleste <voidness12@gmail.com>
2025-04-10 20:50:01 +0900

    [#S12P21E202-113][BE] Multifile 테스트

---
+++ BE/chat/src/main/java/com/ssafy/chat/common/config/MultipartConfig.java
@@ -0,0 +1,19 @@
+package com.ssafy.chat.common.config;
+
+import jakarta.servlet.MultipartConfigElement;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+
+@Configuration
+public class MultipartConfig {
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize(DataSize.ofMegabytes(50));
+        factory.setMaxRequestSize(DataSize.ofMegabytes(50));
+        return factory.createMultipartConfig();
+    }
+}
