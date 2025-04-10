2d997e37f2cf3bb30fd27f7680f7bf0c1b403aff
이수환 <qkqhzz120@naver.com>
2025-04-10 21:20:33 +0900

    [#S12P21E202-154][BE] Refacto: 은행 페이 통합

---
+++ BE/financial/src/main/java/com/ssafy/financial/dto/request/common/FinancialUserInfo.java
@@ -0,0 +1,10 @@
+package com.ssafy.financial.dto.request.common;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+public class FinancialUserInfo {
+    private final String apiKey = "fc1880ecc2444474a54db3592c898761";
+}
