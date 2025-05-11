6a6a37314059444e5a0ef43c8b36173edf20ee59
이수환 <qkqhzz120@naver.com>
2025-05-11 12:34:05 +0900

    Delete ssafy 결제 시스템

--- BE/financial/src/main/java/com/ssafy/financial/dto/request/SimplePasswordVerifyRequest.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.financial.dto.request;
-
-import lombok.Data;
-
-@Data
-public class SimplePasswordVerifyRequest {
-    private Long userId;         // 사용자 ID
-    private String rawPassword;  // 사용자가 입력한 6자리 비밀번호
-}
--- BE/financial/src/main/java/com/ssafy/financial/repository/RegisterProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.financial.repository;
-
-import com.ssafy.financial.entity.RegisterProductEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RegisterProductRepository extends JpaRepository<RegisterProductEntity, Long> {
-}
