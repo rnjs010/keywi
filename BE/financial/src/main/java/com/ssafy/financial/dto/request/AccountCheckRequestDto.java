93507a012b5dba88d02855f8ed2baf3c021fc96c
이수환 <qkqhzz120@naver.com>
2025-05-11 12:34:05 +0900

    Delete ssafy 결제 시스템

--- BE/financial/src/main/java/com/ssafy/financial/dto/request/AccountCheckRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.financial.dto.request;
-
-import lombok.Getter;
-
-@Getter
-public class AccountCheckRequestDto {
-    private String accountNo;
-    private String bankCode;
-}
--- BE/financial/src/main/java/com/ssafy/financial/repository/EscrowTransactionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.financial.repository;
-
-import com.ssafy.financial.entity.EscrowTransactionEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EscrowTransactionRepository extends JpaRepository<EscrowTransactionEntity, Long> {
-
-}
